b79f73633d81650bf554f5862fd947489071fb38
Shojib H. Ruhan <si.ruhan09@gmail.com>
2025-11-23 15:18:59 +0600

    implement form

--- src/part_2/LogInForm.jsx
+++ src/part_2/LogInForm.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+import Field from "./components/Field";
+import FieldSet from "./components/FieldSet";
 
 const LogInForm = () => {
-  return <></>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const submitForm = (formData) => {
+    console.log(formData);
+  };
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <form onSubmit={handleSubmit(submitForm)}>
+        <FieldSet label="Enter Your Details">
+          <Field label="Email" error={errors.email}>
+            <input
+              {...register("email", { required: "Email is required." })}
+              className={`border ${
+                errors.email ? "border-red-500" : "border-gray-200"
+              } box-border p-2 rounded-md w-[300px]`}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email address"
+            />
+          </Field>
+          <Field label="Password" error={errors.password}>
+            <input
+              {...register("password", {
+                required: "Password is required",
+                minLength: {
+                  value: 6,
+                  message: "Password must be atleast 6 charecter long.",
+                },
+              })}
+              className={`border ${
+                errors.email ? "border-red-500" : "border-gray-200"
+              } box-border p-2 rounded-md w-[300px]`}
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter your password"
+            />
+          </Field>
+        </FieldSet>
+        <Field>
+          <button className="text-base text-white p-1 bg-purple-500 border rounded-lg cursor-pointer">
+            LogIn
+          </button>
+        </Field>
+      </form>
+    </div>
+  );
 };
 
 export default LogInForm;
--- src/part_2/components/Field.jsx
+++ src/part_2/components/Field.jsx
@@ -3,10 +3,14 @@ import React from "react";
 const Field = ({ label, children, htmlFor, error }) => {
   const id = htmlFor || getChildId(children);
   return (
-    <div>
-      {label && <label htmlFor={id}>{label}</label>}
+    <div className="flex flex-col items-start justify-start mt-2 mr-2 w-full p-0">
+      {label && (
+        <label htmlFor={id} className="mb-1">
+          {label}
+        </label>
+      )}
       {children}
-      {!!error && <div>{error.message}</div>}
+      {!!error && <div className="text-red-500">{error.message}</div>}
     </div>
   );
 };
@@ -14,7 +18,8 @@ const Field = ({ label, children, htmlFor, error }) => {
 const getChildId = (children) => {
   const child = React.Children.only(children);
 
-  if ("id" in child?.proops) {
+  // eslint-disable-next-line no-unsafe-optional-chaining
+  if ("id" in child?.props) {
     return child.props.id;
   }
 };
